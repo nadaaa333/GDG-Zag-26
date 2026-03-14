b6c4c11c37b425d67cd5cab78938e02f71564f4a
nadaaa333 <nadaaa123468@gmail.com>
2026-03-14 22:44:10 +0200

    Add files via upload

---
+++ task-js-day5-GDG/day5.js
@@ -0,0 +1,110 @@
+/*
+//TASK(1) challeng(1)
+let mydiv=document.querySelector("div");
+console.log(mydiv);
+
+mydiv=document.querySelector("#elzero");
+console.log(mydiv);
+
+mydiv=document.querySelector(".element");
+console.log(mydiv);
+
+mydiv=document.querySelector("[name='js']");
+console.log(mydiv);
+
+mydiv=document.querySelector("#elzero");
+console.log(mydiv);
+
+console.log(mydiv);
+
+mydiv=document.querySelectorAll("#elzero");
+console.log(mydiv);
+
+mydiv=document.querySelectorAll(".element");
+console.log(mydiv);
+
+mydiv=document.querySelectorAll("[name='js']");
+console.log(mydiv);
+
+mydiv=document.querySelectorAll("#elzero");
+console.log(mydiv);
+
+mydiv=document.getElementById("elzero");
+console.log(mydiv);
+
+mydiv=document.getElementsByClassName("element");
+console.log(mydiv);
+
+mydiv=document.getElementsByName("js");
+console.log(mydiv);
+
+mydiv=document.getElementsByTagName("div");
+console.log(mydiv);
+
+console.log(document.body.firstElementChild);
+
+console.log(document.body.childNodes[1]);
+
+console.log(document.body.children[0]);
+*/
+
+/*
+ //challenge(2).
+ 
+  let myElemnt2=document.querySelectorAll('[decoding="async"]');
+  for(let i=0;i<myElemnt2.length;i++){
+ myElemnt2[i].setAttribute("src","https://elzero.org/wp-content/themes/elzero/imgs/logo.png");
+  myElemnt2[i].setAttribute("alt","Elzero Logo");
+  console.log(myElemnt2);
+  }
+  */
+ /*
+ //3
+ let myinput = document.querySelector("input");
+let mydiv = document.querySelector("div");
+
+myinput.addEventListener("input", function () {
+
+  let dollar = myinput.value;
+  let pound = (dollar * 15.6).toFixed(2);
+
+  mydiv.textContent = `${dollar} USD Dollar = ${pound} Egyptian Pound`;
+
+});
+*/
+
+//4
+/*
+ let myDiv1=document.getElementsByClassName("one")[0];
+ let myDiv2=document.getElementsByClassName("two")[0];
+
+ myDiv1.setAttribute("title","one");
+ myDiv2.setAttribute("title","two");
+ myDiv1.textContent = "One";
+ myDiv2.textContent= "Two 2";
+ console.log(myDiv1);
+ console.log(myDiv2);
+ */
+/*
+//5
+
+  let myimg=document.getElementsByTagName("img");
+ 
+  for(let i=0;i<myimg.length;i++){
+
+  if(myimg[i].hasAttribute("alt")){
+    myimg[i].setAttribute("alt","Old")
+  }
+  else{
+      myimg[i].setAttribute("alt","Elzero New")
+  }
+  console.log(myimg);
+  }
+*/
+
+
+  
+
+
+
+
